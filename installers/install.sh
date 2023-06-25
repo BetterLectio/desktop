90ded0610416c6175a74ddbc3e6ea31391612811
jona799t <jonathan.b.skettrup@gmail.com>
2023-06-25 18:23:35 +0200

    Linux installer and updater

--- installers/install.sh
+++ installers/install.sh
@@ -1,9 +1,16 @@
+mkdir betterlectio_temp
+cd betterlectio_temp
+
 sudo rm -rf /usr/share/betterlectio/
+wget https://github.com/BetterLectio/desktop-compiler/releases/download/0.0.1/betterlectio-linux.zip
 sudo unzip betterlectio-linux.zip -d /usr/share/betterlectio/
 
 sudo mkdir /usr/share/app-logos/
 wget https://raw.githubusercontent.com/BetterLectio/betterLectio/main/static/favicon.png --output-document=betterlectio.png
 sudo mv betterlectio.png /usr/share/app-logos/
 
-#wget https://raw.githubusercontent.com/BetterLectio/betterLectio/dev/buildData/betterlectio.desktop
+wget https://raw.githubusercontent.com/BetterLectio/desktop-compiler/main/installers/betterlectio.desktop
 sudo mv betterlectio.desktop /usr/share/applications/
+
+cd ..
+sudo rm -rf betterlectio_temp
